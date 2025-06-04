53303d8f5955bb3fa7a9bfe22b2ae57af02ee3ba
Alireza Maleky <maleky.dev@gmail.com>
2025-06-04 15:30:56 +0330

    fix: prevent unnecessary restarts of hiddify-cli and warp-plus services

--- src/usr/share/v2ray/url-test.sh
+++ src/usr/share/v2ray/url-test.sh
@@ -3,9 +3,13 @@
 TEST_URL="http://www.gstatic.com/generate_204"
 
 if ! curl --socks5 127.0.0.1:8086 --silent --output "/dev/null" "$TEST_URL"; then
-  /etc/init.d/warp-plus restart
+  if /etc/init.d/warp-plus status | grep -q "running"; then
+    /etc/init.d/warp-plus restart
+  fi
 fi
 
 if ! curl --socks5 127.0.0.1:12334 --silent --output "/dev/null" "$TEST_URL"; then
-  /etc/init.d/hiddify-cli restart
+  if /etc/init.d/hiddify-cli status | grep -q "running"; then
+    /etc/init.d/hiddify-cli restart
+  fi
 fi
