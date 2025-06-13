8726a4af0e562dbd610b0592a345039db5724b8f
Alireza Maleky <maleky.dev@gmail.com>
2025-06-13 12:41:19 +0330

    perf: url test max time

--- src/root/scripts/url-test.sh
+++ src/root/scripts/url-test.sh
@@ -2,13 +2,13 @@
 
 TEST_URL="http://www.gstatic.com/generate_204"
 
-if ! curl --socks5 127.0.0.1:8086 --silent --output "/dev/null" "$TEST_URL"; then
+if ! curl --socks5 127.0.0.1:8086 --silent --max-time 5 --output "/dev/null" "$TEST_URL"; then
   if /etc/init.d/warp-plus status | grep -q "running"; then
     /etc/init.d/warp-plus restart
   fi
 fi
 
-if ! curl --socks5 127.0.0.1:12334 --silent --output "/dev/null" "$TEST_URL"; then
+if ! curl --socks5 127.0.0.1:12334 --silent --max-time 5 --output "/dev/null" "$TEST_URL"; then
   if /etc/init.d/hiddify-cli status | grep -q "running"; then
     /etc/init.d/hiddify-cli restart
   fi
