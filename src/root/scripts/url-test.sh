9a6582e8d56adfd88a9514b4c28866705fda0d60
Alireza Maleky <maleky.dev@gmail.com>
2025-07-03 23:13:35 +0330

    feat: url test add retry

--- src/root/scripts/url-test.sh
+++ src/root/scripts/url-test.sh
@@ -3,7 +3,7 @@
 TEST_URL="https://1.1.1.1/cdn-cgi/trace/"
 
 if /etc/init.d/hiddify-cli enabled; then
-  if ! curl --socks5 127.0.0.1:12334 --silent --max-time 10 --output "/dev/null" "$TEST_URL"; then
+  if ! curl --max-time 10 --retry 2 --socks5 127.0.0.1:12334 --silent --output "/dev/null" "$TEST_URL"; then
     echo "ERROR: Hiddify proxy connectivity test failed. Restarting hiddify-cli service..."
     /etc/init.d/hiddify-cli restart
   else
@@ -14,7 +14,7 @@ else
 fi
 
 if /etc/init.d/warp-plus enabled; then
-  if ! curl --socks5 127.0.0.1:8086 --silent --max-time 10 --output "/dev/null" "$TEST_URL"; then
+  if ! curl --max-time 10 --retry 2 --socks5 127.0.0.1:8086 --silent --output "/dev/null" "$TEST_URL"; then
     echo "ERROR: WARP proxy connectivity test failed. Clearing cache and restarting warp-plus service..."
     rm -rfv /.cache/warp-plus/
     /etc/init.d/warp-plus restart
@@ -26,7 +26,7 @@ else
 fi
 
 if /etc/init.d/ssh-proxy enabled; then
-  if ! curl --socks5 127.0.0.1:1080 --silent --max-time 10 --output "/dev/null" "$TEST_URL"; then
+  if ! curl --max-time 10 --retry 2 --socks5 127.0.0.1:1080 --silent --output "/dev/null" "$TEST_URL"; then
     echo "ERROR: SSH proxy connectivity test failed. Restarting ssh-proxy service..."
     rm -fv /root/.ssh/known_hosts
     /etc/init.d/ssh-proxy restart
