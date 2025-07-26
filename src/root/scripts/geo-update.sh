4e581b6939301cb6440fdf7f3c0b7a11517964f4
Alireza Maleky <maleky.dev@gmail.com>
2025-07-26 18:57:17 +0330

    perf: speedup geo files re-download

--- src/root/scripts/geo-update.sh
+++ src/root/scripts/geo-update.sh
@@ -3,10 +3,27 @@
 if [ ! -d "/usr/share/v2ray" ]; then mkdir -p "/usr/share/v2ray"; fi
 if [ ! -d "/usr/share/singbox" ]; then mkdir -p "/usr/share/singbox"; fi
 
+download() {
+  FILE="$1"
+  URL="$2"
+  REMOTE_SIZE=$(curl -sI "$URL" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
+
+  if [ -f "$FILE" ]; then
+    LOCAL_SIZE=$(wc -c < "$FILE" | tr -d ' ')
+  else
+    LOCAL_SIZE=0
+  fi
+
+  if [ "$REMOTE_SIZE" != "$LOCAL_SIZE" ]; then
+    curl -L -o "$FILE" "$URL"
+  fi
+}
+
+
 # ip
-curl -L -o /usr/share/v2ray/geoip.dat "https://cdn.jsdelivr.net/gh/chocolate4u/Iran-v2ray-rules@release/geoip.dat"
-curl -L -o /usr/share/singbox/geoip.db "https://cdn.jsdelivr.net/gh/chocolate4u/Iran-sing-box-rules@release/geoip.db"
+download "/usr/share/v2ray/geoip.dat" "https://cdn.jsdelivr.net/gh/chocolate4u/Iran-v2ray-rules@release/geoip.dat"
+download "/usr/share/singbox/geoip.db" "https://cdn.jsdelivr.net/gh/chocolate4u/Iran-sing-box-rules@release/geoip.db"
 
 # domain
-curl -L -o /usr/share/v2ray/geosite.dat "https://cdn.jsdelivr.net/gh/chocolate4u/Iran-v2ray-rules@release/geosite.dat"
-curl -L -o /usr/share/singbox/geosite.db "https://cdn.jsdelivr.net/gh/chocolate4u/Iran-sing-box-rules@release/geosite.db"
+download "/usr/share/v2ray/geosite.dat" "https://cdn.jsdelivr.net/gh/chocolate4u/Iran-v2ray-rules@release/geosite.dat"
+download "/usr/share/singbox/geosite.db" "https://cdn.jsdelivr.net/gh/chocolate4u/Iran-sing-box-rules@release/geosite.db"
