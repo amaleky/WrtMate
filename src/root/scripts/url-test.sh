65a01a0a25618af36cd0c1cc13dd86885a16d80a
Alireza Maleky <maleky.dev@gmail.com>
2025-10-09 14:51:44 +0330

    fix: improve ghost connectivity test logic

--- src/root/scripts/url-test.sh
+++ src/root/scripts/url-test.sh
@@ -13,18 +13,18 @@ if [ "$(uci get passwall2.@global[0].enabled 2>/dev/null)" = "1" ]; then
 fi
 
 if /etc/init.d/ghost enabled; then
-  if [ "$(logread | grep "run.sh\[$(pgrep -f '/root/ghost/run.sh')\]" | grep -c "ERROR")" -gt 50 ] || \
+  while [ "$(logread | grep "run.sh\[$(pgrep -f '/root/ghost/run.sh')\]" | grep -c "ERROR")" -gt 50 ] || \
     [ "$(curl -s -L -I --max-time 1 --retry 3 --socks5-hostname "127.0.0.1:9802" -o "/dev/null" -w "%{http_code}" "https://telegram.org/")" -ne 200 ] || \
     [ "$(curl -s -L -I --max-time 1 --retry 3 --socks5-hostname "127.0.0.1:9802" -o "/dev/null" -w "%{http_code}" "https://www.youtube.com/")" -ne 200 ] || \
     [ "$(curl -s -L -I --max-time 1 --retry 3 --socks5-hostname "127.0.0.1:9802" -o "/dev/null" -w "%{http_code}" "https://firebase.google.com/")" -ne 200 ] || \
-    [ "$(curl -s -L -I --max-time 1 --retry 3 --socks5-hostname "127.0.0.1:9802" -o "/dev/null" -w "%{http_code}" "https://developer.android.com/")" -ne 200 ]; then
+    [ "$(curl -s -L -I --max-time 1 --retry 3 --socks5-hostname "127.0.0.1:9802" -o "/dev/null" -w "%{http_code}" "https://developer.android.com/")" -ne 200 ]; do
     echo "❌ ghost connectivity test failed"
     sed -i '1d' "/root/ghost/configs.conf"
     /etc/init.d/ghost restart
     /etc/init.d/scanner start
-  else
-    echo "✅ ghost connectivity test passed"
-  fi
+    sleep 5
+  done
+  echo "✅ ghost connectivity test passed"
 else
   echo "⚠️ ghost is not running"
 fi
