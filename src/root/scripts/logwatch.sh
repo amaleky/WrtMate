bd4b0fe15c54290db9b91a5bfbdb2d761c241035
Alireza Maleky <maleky.dev@gmail.com>
2026-01-07 19:21:55 +0330

    fix: ghost logwatch condition

--- src/root/scripts/logwatch.sh
+++ src/root/scripts/logwatch.sh
@@ -4,9 +4,9 @@ GHOST_ERRORS=0
 BALANCER_ERRORS=0
 
 logread -f | while IFS= read -r LINE; do
-  if echo "$LINE" | grep "run.sh\[$(pgrep -f '/usr/bin/sing-box run -c /root/ghost/configs.json')\]" | grep -q "ERROR"; then
+  if echo "$LINE" | grep "sing-box\[$(pgrep -f '/usr/bin/sing-box run -c /root/ghost/configs.json')\]" | grep -q "ERROR"; then
     GHOST_ERRORS=$((GHOST_ERRORS + 1))
-    echo "Error detected. Counter: $GHOST_ERRORS"
+    echo "Error detected (ghost). Counter: $GHOST_ERRORS"
     if [ "$GHOST_ERRORS" -gt 10 ]; then
       echo "Restarting ghost due to excessive errors..."
       /etc/init.d/ghost restart
@@ -15,7 +15,7 @@ logread -f | while IFS= read -r LINE; do
   fi
   if echo "$LINE" | grep "run.sh\[$(pgrep -f '/root/balancer/run.sh')\]" | grep -q "ERROR"; then
     BALANCER_ERRORS=$((BALANCER_ERRORS + 1))
-    echo "Error detected. Counter: $BALANCER_ERRORS"
+    echo "Error detected (balancer). Counter: $BALANCER_ERRORS"
     if [ "$BALANCER_ERRORS" -gt 10 ]; then
       echo "Restarting balancer due to excessive errors..."
       /etc/init.d/balancer restart
