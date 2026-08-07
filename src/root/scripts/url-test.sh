608fb1167887420e7930c82cc7d86d2b77dacfa1
Alireza Maleky <maleky.dev@gmail.com>
2026-08-07 21:41:42 +0330

    feat: psiphon region selector

--- src/root/scripts/url-test.sh
+++ src/root/scripts/url-test.sh
@@ -5,16 +5,6 @@ if [ -f "/tmp/passwall_install.lock" ]; then
   exit 0
 fi
 
-get_retry_count() {
-  FILE="/tmp/$1.count"
-  cat "$FILE" 2>/dev/null || echo 0
-}
-
-set_retry_count() {
-  FILE="/tmp/$1.count"
-  echo "$(($(get_retry_count "$1") + 1))" >"$FILE"
-}
-
 test_socks_port() {
   SOCKS_PORT=$1
   URL=$2
@@ -26,29 +16,57 @@ test_socks_port() {
 }
 
 test_serverless() {
+  SERVICE="serverless"
+  SERVERLESS_CONFIG="/root/xray/serverless.json"
+  SERVERLESS_SUBSCRIPTION="/root/xray/subscription.json"
+
+  if ! /etc/init.d/"$SERVICE" enabled; then
+    return
+  fi
+
   if test_socks_port "10808" "https://www.youtube.com"; then
     echo "✅ serverless connectivity test passed"
   else
-    SERVERLESS_CONFIG="/root/xray/serverless.json"
-    SERVERLESS_SUBSCRIPTION="/root/xray/subscription.json"
-    FOUND_WORKING=0
-    i=$(($(jq '. | length' "$SERVERLESS_SUBSCRIPTION") - 1))
-    while [ "$i" -ge 0 ]; do
+    i=$(jq '. | length' "$SERVERLESS_SUBSCRIPTION")
+    while [ "$i" -gt 0 ]; do
+      i=$((i - 1))
       jq ".[$i]" "$SERVERLESS_SUBSCRIPTION" > "$SERVERLESS_CONFIG"
       echo "Testing serverless [$(jq -r '.remarks' "$SERVERLESS_CONFIG")]"
-      /etc/init.d/serverless restart
+      /etc/init.d/"$SERVICE" restart
       sleep 5
       if test_socks_port "10808" "https://www.youtube.com"; then
         echo "✅ serverless connectivity test passed"
-        FOUND_WORKING=1
         break
       fi
-      i=$((i - 1))
     done
-    if [ "$FOUND_WORKING" -eq 0 ]; then
-      echo "❌ serverless connectivity test failed"
-      /etc/init.d/serverless stop
-    fi
+  fi
+}
+
+test_psiphon() {
+  SERVICE="psiphon"
+  NODE="Psiphon"
+  PSIPHON_CONFIG="/root/psiphon/client.config"
+
+  if ! /etc/init.d/"$SERVICE" enabled; then
+    return
+  fi
+
+  if test_socks_port "9804" "https://1.1.1.1/cdn-cgi/trace/"; then
+    echo "✅ $NODE connectivity test passed"
+    return
+  fi
+
+  echo "❌ $NODE connectivity test failed"
+
+  set -- AT AU BE CA CH CZ DE DK ES FR GB ID IN IT JP LT NL NO PL RO RS SE SG US
+  REGION_INDEX=$(awk 'BEGIN { srand(); print int(rand() * 24) + 1 }')
+  shift $((REGION_INDEX - 1))
+  EGRESS_REGION="$1"
+
+  if jq --arg region "$EGRESS_REGION" '.EgressRegion = $region' "$PSIPHON_CONFIG" >"${PSIPHON_CONFIG}.tmp"; then
+    mv "${PSIPHON_CONFIG}.tmp" "$PSIPHON_CONFIG"
+    echo "Changing $NODE egress region to $EGRESS_REGION"
+    /etc/init.d/"$SERVICE" restart
   fi
 }
 
@@ -56,41 +74,29 @@ test_service() {
   SERVICE="$1"
   NODE="$2"
   PORT="$3"
-  AUTO_STOP="$4"
 
   if ! /etc/init.d/"$SERVICE" enabled; then
     return
   fi
 
-  if [ "$(get_retry_count "$SERVICE")" -le 5 ] || [ "$(uci get passwall2.Splitter.default_node)" = "$NODE" ] || [ "$(uci get passwall2.Splitter.Sanction)" = "$NODE" ]; then
-    if ! test_socks_port "$PORT" "https://1.1.1.1/cdn-cgi/trace/"; then
-      echo "❌ $NODE connectivity test failed"
-      case "$SERVICE" in
-        warp-plus) rm -rfv /.cache/warp-plus ;;
-        ssh-proxy) rm -fv /root/.ssh/known_hosts ;;
-      esac
-      if [ "$SERVICE" != "scanner" ]; then
-        set_retry_count "$SERVICE"
-      fi
-      /etc/init.d/"$SERVICE" restart
-    else
-      echo "✅ $NODE connectivity test passed"
-    fi
+  if test_socks_port "$PORT" "https://1.1.1.1/cdn-cgi/trace/"; then
+    echo "✅ $NODE connectivity test passed"
   else
-    if [ "$AUTO_STOP" != "false" ]; then
-      if /etc/init.d/"$SERVICE" running; then
-        /etc/init.d/"$SERVICE" stop
-      fi
-    fi
+    echo "❌ $NODE connectivity test failed"
+    case "$SERVICE" in
+      warp-plus) rm -rfv /.cache/warp-plus ;;
+      ssh-proxy) rm -fv /root/.ssh/known_hosts ;;
+    esac
+    /etc/init.d/"$SERVICE" restart
   fi
 }
 
 main() {
-  test_service "scanner" "Scanner" 9802 "false"
-  test_service "warp-plus" "WarpPlus" 9803 "true"
-  test_service "psiphon" "Psiphon" 9804 "false"
-  test_service "tor" "Tor" 9805 "true"
-  test_service "ssh-proxy" "SshProxy" 9806 "true"
+  test_service "scanner" "Scanner" 9802
+  test_service "warp-plus" "WarpPlus" 9803
+  test_service "tor" "Tor" 9805
+  test_service "ssh-proxy" "SshProxy" 9806
+  test_psiphon
   test_serverless
 }
 
